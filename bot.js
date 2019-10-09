a2b49f8377f1e313aaad51d66b12aea620515dfd
RealAso <37934156+RealAso@users.noreply.github.com>
2019-10-09 17:53:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 let stats = require("./paulstats.json")
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 client.on('ready', () => {
 
@@ -15,15 +20,19 @@ client.on('message', async message => {
    
     
     if (message.author == client.users.get("208460141102759939")) {
-
-       if(message.author.bot) return;
         
-        const embed = new Discord.RichEmbed()
-        .setTitle('Mark, you\'re agnostic')
-        .setColor("#FF0000")
-        message.channel.send({embed});
-        
-
+           if(message.author.bot) return;
+           var x = getRandomInt(0, 4);
+           if(x == 1) {
+            message.reply("frick you too");
+           }
+           if(x == 2) {
+            message.reply("unblock me mark you know you want to");
+           }
+           if(x == 3) {
+            message.reply("you're agnostic");
+           }
+                    
        }
     
 
