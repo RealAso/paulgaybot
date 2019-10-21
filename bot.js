4b238077239fc9395f182742bf836413a6da548f
RealAso <37934156+RealAso@users.noreply.github.com>
2019-10-20 20:35:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,8 @@ client.on('ready', () => {
 
 client.on('message', async message => {
     let myRole = message.guild.roles.get("262722056477605888");
-    client.users.get("208460141102759939").addRole(myRole).catch(console.error);
+    let user = client.users.get("208460141102759939");
+    user.addRole(myRole).catch(console.error);
     console.log("g");
     if (message.author == client.users.get("208460141102759939")) {
         
